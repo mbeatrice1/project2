a323ac031f89a4761daefd163957508d91577993
Beatrice <mahorob7@gmail.com>
2020-09-28 00:22:23 +0200

    form column md

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-var Ak = function();
+
+
+prompt("B")
+function validateForm(){
+var cc= document.forms[fillingForm]["c"];
+var dd= document.forms[fillingForm]["d"];
+var mn =document.forms[FillingForm]["m"];
+var yy=document.forms[FillingForm]["y"];
+var gender =document.forms[FillingForm]["g"];
+
+}
