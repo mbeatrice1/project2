2a0125b763d6d8f665af0441e0398339e54153d5
Beatrice <mahorob7@gmail.com>
2020-09-29 14:21:04 +0200

    added function

--- js/index.js
+++ js/index.js
@@ -1,9 +1,110 @@
 
-var jumpingJacks= function(numberOfTimes,whatToDo){
-    document.getElementById("btnclick").onclick= jumpingJacks;
-    for(var i =1; i<numberOfTimes; i++){
-      console.log(i+" "+whatToDo);
+alert(bornday);
+
+function akanName(){
+
+
+
+var dd= document.getElementById("d").value;
+var mn=document.getElementById("m").value;
+var cc=document.getElementById("c").value;
+var yy=document.getElementById("y").value;
+var gender=document.getElementById("g").value;
+var bornday=  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mn+1)/10)) + dd ) % 7;
+var rea= Math.trunc(bornday);
+start:
+
+if(dd<1 || day>31 && mn<1 || mn>12){
+
+  alert("Wrong number");
+  goto :start;
+  
+
+}else (gender == "male"){
+  switch(rea){
+
+case 1:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+
+case 2:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+
+case 3:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+case 4:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+
+case 5:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+
+case 6:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+
+case 7:
+
+  document.write("You have born on wednesday and your Akhan KWADO");
+break;
+ default:
+  document.write("No available choices")
+
+  }else if(gender == "female")
+
+  {
+    switch(rea){
+
+      case 1:
+
+        document.write("You have born on wednesday and your Akhan KWASI");
+      break;
+      
+      case 2:
+      
+        document.write("You have born on wednesday and your Akhan KWASI");
+      break;
+      
+      case 3:
+      
+        document.write("You have born on wednesday and your Akhan KWASI");
+      break;
+      case 4:
+      
+        document.write("You have born on wednesday and your Akhan KWADO");
+      break;
+      
+      case 5:
+      
+        document.write("You have born on wednesday and your Akhan KWASI");
+      break;
+      
+      case 6:
+      
+        document.write("You have born on wednesday and your Akhan KWASI");
+      break;
+      
+      case 7:
+      
+        document.write("You have born on wednesday and your Akhan KWASI");
+      break;
+       default:
+        document.write("No available choices")
+
     }
-   
- };
- jumpingJacks(13,"Jump over the puddle");
+  }
+
+}
+
+
+
+}
